1f7f36afe7fbc29a5c518097bd30524585d44a62
Olha Maievska <olhamaievska91@gmail.com>
2024-05-19 22:40:26 +0200

    Updates

--- static/js/258.c95ff5da.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgid_group_react=self.webpackChunkgid_group_react||[]).push([[258],{6258:(e,r,n)=>{n.r(r),n.d(r,{default:()=>c});var s=n(3216),a=n(579);const c=()=>{const e=(0,s.Zp)();return(0,a.jsxs)("div",{className:"pageEror",children:[(0,a.jsx)("h1",{children:"Error 404. Page not found"}),(0,a.jsx)("button",{className:"pageEror__btn",onClick:function(){e("/")},children:"Home page"})]})}}}]);
-//# sourceMappingURL=258.c95ff5da.chunk.js.map
---
+++ static/js/574.37b63bc3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgid_group_react=self.webpackChunkgid_group_react||[]).push([[574],{574:(e,r,n)=>{n.r(r),n.d(r,{default:()=>c});var s=n(3216),a=n(579);const c=()=>{const e=(0,s.Zp)();return(0,a.jsxs)("div",{className:"pageEror",children:[(0,a.jsx)("h1",{children:"Error 404. Page not found"}),(0,a.jsx)("button",{className:"pageEror__btn",onClick:function(){e("/")},children:"Home page"})]})}}}]);
+//# sourceMappingURL=574.37b63bc3.chunk.js.map
